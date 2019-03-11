29d38e08be8124d9e23fff9242d66a05211a4eca
Pranay Solse <solsepranay0@gmail.com>
2019-03-11 15:53:15 +0530

    all logins completed

--- studentbook/auth.py
+++ studentbook/auth.py
@@ -44,7 +44,8 @@ def register_student():
 		flash(error)
 		if error is not None:
 			print(error)
-	return render_template("register_student.html",title="Register-Student",form=form)		
+	return render_template("register_student.html",title="Register-Student",form=form)	
+
 @bp.route('/login_s', methods=('GET','POST'))
 def login_student():
 	form = forms.LoginForm()
@@ -69,7 +70,7 @@ def login_student():
 			user = db.execute(
 				'SELECT * FROM student WHERE username = ?',(username,)
 			).fetchone()
-			print(user)
+			for i in user :print(i)
 		if user is None:
 			error = 'incorrect username'
 		elif not check_password_hash(user['password'],password):	
@@ -86,8 +87,162 @@ def login_student():
 		
 	return render_template('login.html',title='login student',form=form)	
 
+#------------------------------------------------------------------------------------------
+#////////////////////////////////////////////////////////////////////////////////////////
 
-#<label class="checkbox-inline">Login As:
-#		        			{{form.user_type(type="select" , class="custom-select my-1 mr-sm-2")}}
-#		        	</label><br><br><br>
-# <label class="checkbox-inline">{{ form.remember(type="checkbox") }} {{ form.remember.label() }} 
+@bp.route('/register_t',methods=('GET','POST'))
+def register_teacher():
+	form = forms.RegisterFormte()
+
+	if request.method == 'POST':
+		print(str(request.data))
+		username = request.form['username']
+		print(username)
+		password = request.form['password']
+		db = get_db()
+		error = None
+
+		if not username:
+			error = 'username is required.'
+		elif not password:
+			error = 'password is required'	
+		elif db.execute(
+			'SELECT id FROM teacher WHERE username = ?',(username,)
+		).fetchone() is not None:	
+			error = f'user {username} is already registered'
+			print(error)
+			return redirect(url_for('auth.login_teacher'))
+		if error is None:	
+			db.execute(
+				'INSERT INTO teacher (username,password) VALUES (?,?)',
+				(username, generate_password_hash(password))
+			)
+			db.commit()
+			print("commited")
+			return redirect(url_for('auth.login_teacher'))
+		print("flashing now")	
+		flash(error)
+		if error is not None:
+			print(error)
+	return render_template("register_teacher.html",title="Register-teacher",form=form)		
+
+@bp.route('/login_t', methods=('GET','POST'))
+def login_teacher():
+	
+	form = forms.LoginForm()
+	if request.method == 'POST':
+		flash("login form")
+		print(str(request.data))
+		username = request.form['username']
+		password = request.form['password']
+		user_type = request.form['user_type']
+		db = get_db()
+		error = None
+
+		if username == None:
+			error = 'username is required '
+
+		elif password == None:	
+			error = 'password is required'
+		elif user_type == None:
+			error = 'user type is required'
+		else :
+			user = db.execute(
+				'SELECT * FROM teacher WHERE username = ?',(username,)
+			).fetchone()
+			
+		if user is None:
+			error = 'incorrect username'
+		elif not check_password_hash(user['password'],password):	
+			error = 'Incorrect password'
+		print(error)	
+		if error is None:
+			print("error is None")
+			session.clear()
+			session['user_id']=user['id']
+			return redirect(url_for('index'))
+			return None
+		print("flashing now")
+		flash(error)
+		
+	return render_template('login.html',title='login teacher',form=form)
+
+#----------------------------------------------------------------------------------------
+#////////////////////////////////////////////////////////////////////////////////////////
+@bp.route('/register_c',methods=('GET','POST'))
+def register_comitteehead():
+	form = forms.RegisterFormco()
+
+	if request.method == 'POST':
+		print(str(request.data))
+		username = request.form['username']
+		print(username)
+		password = request.form['password']
+		db = get_db()
+		error = None
+
+		if not username:
+			error = 'username is required.'
+		elif not password:
+			error = 'password is required'	
+		elif db.execute(
+			'SELECT id FROM comitteehead WHERE username = ?',(username,)
+		).fetchone() is not None:	
+			error = f'user {username} is already registered'
+			print(error)
+			return redirect(url_for('auth.login_committee'))
+		if error is None:	
+			db.execute(
+				'INSERT INTO comitteehead (username,password) VALUES (?,?)',
+				(username, generate_password_hash(password))
+			)
+			db.commit()
+			print("commited")
+			return redirect(url_for('auth.login_committee'))
+		print("flashing now")	
+		flash(error)
+		if error is not None:
+			print(error)
+	return render_template("register_committee.html",title="Register-teacher",form=form)		
+
+@bp.route('/login_c', methods=('GET','POST'))
+def login_committee():
+	
+	form = forms.LoginForm()
+
+	if request.method == 'POST':
+		flash("login form")
+		print(str(request.data))
+		username = request.form['username']
+		password = request.form['password']
+		user_type = request.form['user_type']
+		db = get_db()
+		error = None
+
+		if username == None:
+			error = 'username is required '
+
+		elif password == None:	
+			error = 'password is required'
+		elif user_type == None:
+			error = 'user type is required'
+		else :
+			user = db.execute(
+				'SELECT * FROM comitteehead WHERE username = ?',(username,)
+			).fetchone()
+		print(username)	
+		if user is None:
+			error = 'incorrect username'
+		elif not check_password_hash(user['password'],password):	
+			error = 'Incorrect password'
+		print(error)	
+		if error is None:
+			print("error is None")
+			session.clear()
+			session['user_id']=user['id']
+			return redirect(url_for('index'))
+			return None
+		print("flashing now")
+		flash(error)
+		
+	return render_template('login.html',title='login commitee',form=form)
--- studentbook/schema.sql
+++ studentbook/schema.sql
@@ -14,20 +14,21 @@ CREATE TABLE student (
 CREATE TABLE comitteehead (
 	id INTEGER PRIMARY KEY,
 	username TEXT UNIQUE NOT NULL,
-	password TEXT NOT NULL,
-	address TEXT NOT NULL,
-	class TEXT NOT NULL,
-	branch TEXT NOT NULL,
-	year TEXT NOT NULL,
-	role TEXT NOT NULL
+	password TEXT ,
+	address TEXT ,
+	class TEXT ,
+	branch TEXT ,
+	year TEXT ,
+	role TEXT 
 );
 
 CREATE TABLE teacher (
 	id INTEGER PRIMARY KEY,
 	username TEXT UNIQUE NOT NULL,
-	address TEXT NOT NULL,
-	designation TEXT NOT NULL,
-	qualification TEXT NOT NULL,
-	subject TEXT NOT NULL
+	address TEXT ,
+	designation TEXT ,
+	qualification TEXT ,
+	subject TEXT ,
+	password TEXT NOT NULL
 )
 
