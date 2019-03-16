462040b2aae66b9f2ad5f214dd5a0e4c196b699e
Pranay Solse <solsepranay0@gmail.com>
2019-03-16 20:01:56 +0530

    ...

--- studentbook/auth.py
+++ studentbook/auth.py
@@ -1,45 +1,43 @@
 import functools
 from flask import(
-	Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from studentbook.db import get_db
 from studentbook import forms
-bp = Blueprint('auth',__name__,url_prefix='/auth')
+bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/register_s', methods=('GET','POST'))
+
+@bp.route('/register_s', methods=('GET', 'POST'))
 def register_student():
-	form = forms.RegisterFormSt()
-		
-	if request.method == 'POST':
-
-		print(str(request.data))
-		username = request.form['username']
-		print(username)
-		password = request.form['password']
-		db = get_db()
-		error = None
-
-		if not username:
-			error = 'username is required.'
-		elif not password:
-			error = 'password is required'	
-		elif db.execute(
-			'SELECT id FROM student WHERE username = ?',(username,)
-		).fetchone() is not None:	
-			error = f'user {username} is already registered'
-			print(error)
-			return redirect(url_for('auth.login_student'))
-		if error is None:	
-			db.execute(
-				'INSERT INTO student (username,password) VALUES (?,?)',
-				(username, generate_password_hash(password))
-			)
-			db.commit()
-			print("commited")
-			return redirect(url_for('auth.login_student'))
+    form = forms.RegisterFormSt()
+    if request.method == 'POST':
+        print(str(request.data))
+        username = request.form['username']
+        print(username)
+        password = request.form['password']
+        db = get_db()
+        error = None
+        if not username:
+            error = 'username is required.'
+        elif not password:
+            error = 'password is required'
+        elif db.execute(
+            'SELECT id FROM student WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f'user {username} is already registered'
+            print(error)
+            return redirect(url_for('auth.login_student'))
+        if error is None:
+            db.execute(
+                'INSERT INTO student (username,password) VALUES (?,?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            print("commited")
+            return redirect(url_for('auth.login_student'))
 		print("flashing now")	
 		flash(error)
 		if error is not None:
@@ -245,4 +243,8 @@ def login_committee():
 		print("flashing now")
 		flash(error)
 		
+@bp.route('/logout_c',methods='GET','POST')
+def logout_comittee():		
+	session.pop('username',None)
+	return redirect(url_for('home'))
 	return render_template('login.html',title='login commitee',form=form)
