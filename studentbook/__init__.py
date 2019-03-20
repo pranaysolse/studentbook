ebfb0f3a47f03dc60762c1d72fb42e64a37bfa1f
Mayank <mt281999@gmail.com>
2019-03-20 21:17:53 +0530

    made changes to __init__ page

--- studentbook/__init__.py
+++ studentbook/__init__.py
@@ -1,78 +1,79 @@
 from flask import Flask, g, render_template, request
-from .forms import RegisterFormSt , RegisterFormte , RegisterFormco , LoginForm
+from .forms import RegisterFormSt, RegisterFormte, RegisterFormco
+from .forms import LoginForm as s
 import os
 import os.path
 from . import db
 from . import auth
+from . import admin
+
+
 def create_app(test_config=None):
-	#create factory for app object with test config	
-	app = Flask(__name__,instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='a92cede7a46f3d768a4895932bb47633',
+        DATABASE=os.path.join(app.instance_path, 'studentbook.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+    try:
+        os.makedirs(app.instace_path)
+    except:
+        pass
+
+# print(os.path.abspath(g.__file__))
+    @app.route("/")
+    def home():
+        return render_template("home.html")
+
+    @app.route("/")
+    def index():
+        return render_template("home.html")
 
 
-	app.config.from_mapping(
-		SECRET_KEY='a92cede7a46f3d768a4895932bb47633',
-		DATABASE=os.path.join(app.instance_path,'studentbook.sqlite'),
-	)
+# about page
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
 
-	if test_config is None:
-		#load the instance config
-		app.config.from_pyfile('config.py',silent=True)
-	else:
-		app.config.from_mapping(test_config)
+# contact us page
+    @app.route("/contact")
+    def contact():
+        return render_template("contact.html")
 
-	try:
-		os.makedirs(app.instace_path)
-	except:
-		pass			
-	
-	#print(os.path.abspath(g.__file__))
-	@app.route("/")
-	def home():
-	    return render_template("home.html")
+    # for student
+    @app.route("/register_s", methods=['GET', 'POST'])
+    def register_s():
+        form = RegisterFormSt()
+        return render_template("register_student.html",
+                               title="Register-Student", form=form)
+# return render_template('login.html', title='register-student', form=form)
 
-	@app.route("/user",methods=["POST"])
-	def index():
-	    return render_template("user_page_student.html")
+    # for teacher
+    @app.route("/register_t", methods=['GET', 'POST'])
+    def register_t():
+        form = RegisterFormte()
+        return render_template("register_teacher.html",
+                               title="Register-Teacher", form=form)
 
+    # for committee
+    @app.route("/register_c", methods=['GET', 'POST'])
+    def register_c():
+        form = RegisterFormco()
+        return render_template("register_committee.html",
+                               title="Register-Committee", form=form)
 
-
-	#about page
-	@app.route("/about")
-	def about():
-		return render_template("about.html")
-
-	#contact us page
-	@app.route("/contact")
-	def contact():
-		return render_template("contact.html")
-
-	#for student
-	@app.route("/auth/register_student",methods=['GET','POST'])
-	def register_s():
-		form = RegisterFormSt()
-		return render_template("register_student.html",title="Register-Student",form=form)
-		#return render_template('login.html',title='register-student',form=form)	
-
-	#for teacher
-	@app.route("/auth/register_teacher",methods=['GET','POST'])
-	def register_t():
-		form = RegisterFormte()
-		return render_template("register_teacher.html",title="Register-Teacher",form=form)
-
-	#for committee
-	@app.route("/auth/register_committee",methods=['GET','POST'])
-	def register_c():
-		form = RegisterFormco()
-		return render_template("register_committee.html",title="Register-Committee",form=form)
-
-	#login for all
-	@app.route("/login",methods=['GET','POST'])
-	def login():
-		form = LoginForm()
-		return render_template("login.html",title="Login",form=form)
-
-	db.init_app(app)
-	
-	app.register_blueprint(auth.bp)	
-	
-	return app	
+    # login for all
+    @app.route("/login", methods=['GET', 'POST'])
+    def login():
+        form = s()
+        print(form)
+        return render_template("login.html", title="Login", form=form)
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(admin.bp)
+    return app
